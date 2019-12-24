788fc0adb285f9f6b597e63d4a0585b12e7d7b37
Zelalem <zelalem.antigegn12@gmail.com>
2019-12-24 15:28:48 +0300

    Updates

---
+++ precache-manifest.bfa12fe371eeaae9c84675e06eba1d30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d00feacdcb1ed196d41596ce58026c",
+    "url": "/Monsters/index.html"
+  },
+  {
+    "revision": "86b3468b65a9a13e69c7",
+    "url": "/Monsters/static/css/main.53ba163a.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/Monsters/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86b3468b65a9a13e69c7",
+    "url": "/Monsters/static/js/main.1fa7a312.chunk.js"
+  },
+  {
+    "revision": "e57f55e41ea86d840448",
+    "url": "/Monsters/static/js/runtime-main.eaca0933.js"
+  }
+]);
